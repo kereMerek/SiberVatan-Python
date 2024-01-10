4abc82cd8053ba93e70a386fc542287c90f1915c
Kerem <111786677+kereMerek@users.noreply.github.com>
2024-01-10 19:09:21 +0300

    First commit
    First commit

---
+++ sibervatan.py
@@ -0,0 +1,32 @@
+#print("Hello World")
+
+#örnek 2
+sayi=10
+sayi=100
+print(sayi)
+
+#örnek 3
+yas=10
+YAS=100
+print(YAS)
+
+#örnek 4
+sayi = -7
+print("Mutlak değer:",abs(sayi))
+
+#örnek 5
+us = pow(2,3)
+print("Girdiğiniz sayı üssü:",us)
+
+#örnek 6
+from math import sqrt,ceil,floor
+sayi = 25
+karekok= sqrt(sayi)
+print("Sayınızın karekökü", karekok)
+
+#örnek 7
+ondalik=10.25
+yukari= ceil(ondalik)
+asagi= floor(ondalik)
+print("Yukarı:",yukari)
+print("Aşağı",asagi)
